c9a0b9397c79031774df7bc3bc2f32a229ccaf0f
Harsh Ledwani <ledwani830@gmail.com>
2025-04-23 22:13:19 +0530

    did something don't remember

--- smart_contracts/config.js
+++ smart_contracts/config.js
@@ -7,7 +7,7 @@ const config = {
     
     // Contract configuration
     contract: {
-        address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", // Newly deployed contract address
+        address: "0x5FbDB2315678afecb367f032d93F642f64180aa3", // Newly deployed contract address
         abi: [
             {
                 "inputs": [],
